89b207c6e51f7f3043607a354f3450ef7d372510
shogo-apple <shogo.5643@gmail.com>
2020-11-18 21:38:20 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :birth_date
 
-  validates :nickname, presence: true
-  validates :birth_date, presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates :password, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
 
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
+    VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
+    validates :last_name, :first_name, format: { with: VALID_NAME_REGEX }
 
-  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
-  validates :last_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX }
-
-  VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/.freeze
-  validates :last_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX }
-
+    VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+    validates :last_name_kana, :first_name_kana, format: { with: VALID_KANA_NAME_REGEX }
+  end
   has_many :items
   has_many :purchases
 end
