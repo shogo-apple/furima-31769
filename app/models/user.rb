5a9eb23474295222e911a6883b5b1b6234a45e98
shogo-apple <shogo.5643@gmail.com>
2020-11-18 17:09:47 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :birth_date, presence: true
+ 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, presence: true, length: { minimum: 7 }, format: { with: VALID_PASSWORD_REGEX }
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name, :first_name, presence: true, format: { with: VALID_NAME_REGEX }
+  
+  VALID_KANA_NAME_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: VALID_KANA_NAME_REGEX }
+  
+  has_many :items
+  has_many :purchases
+
 end
