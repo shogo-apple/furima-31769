0d77cfcba64b3b95bd07994791389795a26fffd8
shogo-apple <shogo.5643@gmail.com>
2020-11-19 22:55:39 +0900

    ActiveHash設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :shipping_area
+  belongs_to :shipping_day
   belongs_to :user
   has_one_attached :image
   has_one :purchase
 
   validates :image, presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :shipping_area_id
+    validates :shipping_day_id
+  end
 end
