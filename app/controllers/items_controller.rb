1a6ba318a1e94fb9c63493c98454a066d48715ae
shogo-apple <shogo.5643@gmail.com>
2020-11-20 14:56:53 +0900

    出品表示機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,20 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+ 
+ 
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:introduction,:category_id, :condition_id, :delivery_fee_id, :shipping_area_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,16 +5,22 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :shipping_area
   belongs_to :shipping_day
+  
   belongs_to :user
   has_one_attached :image
   has_one :purchase
 
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_fee_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :shipping_day_id, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 1 }
+  validates :shipping_day_id, numericality: { other_than: 1 }
+  
   validates :image, presence: true
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :condition_id
-    validates :delivery_fee_id
-    validates :shipping_area_id
-    validates :shipping_day_id
-  end
 end
