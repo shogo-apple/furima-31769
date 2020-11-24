e158124f7626f37cf317ddb05c8da2ebef97a0b8
shogo-apple <shogo.5643@gmail.com>
2020-11-24 12:06:38 +0900

    before_actionを使って一括管理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -20,11 +21,9 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -46,4 +45,10 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     redirect_to action: :index if item.user_id != current_user.id
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+
 end
