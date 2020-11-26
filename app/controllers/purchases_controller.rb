0a01443f2505522a52d7e6ba9bc205f2fe110cdd
shogo-apple <shogo.5643@gmail.com>
2020-11-26 15:27:30 +0900

    コントローラー修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,9 +1,20 @@
 class PurchasesController < ApplicationController
-  def index
-    @purchase = Purchase.new
+  def new
+    @purchase_form = PurchaseForm.new
   end
 
   def create
+    @purchase_form = PurchaseForm.new(purchase_params)
+      if @purchase_form.valid?
+        @purchase_form.save
+        redirect_to action: :index
+      else
+        render action: :new
+      end
   end
+
+  private
+ def purchase_params
+  params.require(:purchase_form).permit(:user, :item, :postal_code, :shipping_area_id, :city, :home_number, :apartment_name, :phone_number)
 
 end
