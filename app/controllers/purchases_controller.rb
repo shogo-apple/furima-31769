0264a2d932e773a64c632bc420df58703be1e597
shogo-apple <shogo.5643@gmail.com>
2020-11-27 22:11:42 +0900

    メソッド化

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,19 +1,13 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   def index
     @purchase_form = PurchaseForm.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @purchase_form = PurchaseForm.new(purchase_params)
-    @item = Item.find(params[:item_id])
     if @purchase_form.valid?
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: purchase_params[:token],
-        currency: 'jpy'
-      )
+      pay_jp
       @purchase_form.save
       redirect_to root_path
     else
@@ -22,6 +16,20 @@ class PurchasesController < ApplicationController
   end
 
   private
+
+  def pay_jp
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+    
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def purchase_params
     params.require(:purchase_form).permit(:postal_code, :shipping_area_id, :city, :home_number, :apartment_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
