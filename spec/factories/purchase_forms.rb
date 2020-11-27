dd1ee3fa3be236b4e99451811f1356f1ec478b1d
shogo-apple <shogo.5643@gmail.com>
2020-11-27 19:36:50 +0900

    テストコード

--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -3,9 +3,9 @@ FactoryBot.define do
     postal_code       { '123-4567' }
     shipping_area_id  { 2 }
     city              { '東京都' }
-    home_number      { '1-1' }
-    apartment_name     { '東京ハイツ' }
+    home_number       { '1-1' }
+    apartment_name    { '東京ハイツ' }
     phone_number      { '09012345678' }
-    
+    token             {"tok_abcdefghijk00000000000000000"}
   end
 end
--- spec/models/purchase_form_spec.rb
+++ spec/models/purchase_form_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe PurchaseForm, type: :model do
     before do
       @purchase_form = FactoryBot.build(:purchase_form)
     end
+    context '購入がうまくいくとき' do
+      it 'postal_code、shipping_area_id、city、home_number、phone_number、tokenが存在すれば購入できる。' do
+        expect(@purchase_form).to be_valid
+      end
+    end
    context '購入がうまくいかない時' do
       it 'postal_codeが空だと保存できないこと' do
        @purchase_form.postal_code = nil
@@ -36,12 +41,18 @@ RSpec.describe PurchaseForm, type: :model do
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("Postal code is invalid")
       end
-      # it 'phone_numberが11桁以下出ないと保存できないこと' do
-        # @purchase_form.phone_number = '123456789012'
-        # @purchase_form.valid?
-        # binding.pry
-        # expect(@purchase_form.errors.full_messages).to include("Phone number can't be blank")
-      # end
+      
+       it 'phone_numberが11桁以下出ないと保存できないこと' do
+         @purchase_form.phone_number = '123456789012'
+         @purchase_form.valid?
+         expect(@purchase_form.errors.full_messages).to include("Phone number is too long (maximum is 11 characters)")
+       end
+      
+      it "tokenが空では登録できないこと" do
+        @purchase_form.token = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
